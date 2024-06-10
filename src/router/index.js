16e6797c68f4ca2a4a41cba96f76c68a13185f72
federicot2 <87830686+federicot2@users.noreply.github.com>
2024-06-10 15:15:51 -0500

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import Roble from '../components/pages/RobleView.vue'
+import Panama from '../components/pages/ArbolPanamaView.vue'
+import Home from '../components/pages/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/roble',
+      name: 'roble',
+      component: Roble
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/panama',
+      name: 'panama',
+      component: Panama
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: Home
     }
   ]
 })
