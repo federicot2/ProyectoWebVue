333469f1a08329125c885a69c157222af801170a
federicot2 <87830686+federicot2@users.noreply.github.com>
2024-07-03 12:47:16 -0500

    fix.prueba

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const PROMPT_OBJECTS = [
+    {
+        name: "PC",
+        title: "My beast of a PC",
+        description: "<p>Custom built computer used for gaming and programming. <ul><li>Ryzen 5 2600 | 16 GB RAM</li><li>Geforce GTX 1080 Ti</li><li>HDD + SSD + M.2</li></ul></p>",
+    },
+    {
+        name: "Macbook",
+        title: "MacBook Air M1",
+        description: "<p>MacBook I use for job and school, or when I'm not home.<ul><li>M1 CPU | 13.3\"</li><li>256 GB SSD | 8 GB RAM</li></ul></p>",
+    },
+
+
+    //Con objeto 3D
+    /*{
+        name: "Mouse",
+        title: "SteelSeries Aerox 3",
+        description: "<p>My trusty lightweight mouse. Ideal for gaming and everyday tasks.</p>",
+        model: "hoja.glb"
+    }*/
+]
